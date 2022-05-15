517f2067c51b63a9d8b5e503fe3bfb631750857f
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-05-15 22:41:10 +0300

    add and style login button

--- firstVueProject/vue-project/src/components/LoginForm.vue
+++ firstVueProject/vue-project/src/components/LoginForm.vue
@@ -28,11 +28,26 @@ export default {
             <label class="input" for="repeatPassword">Repeat Password</label>
             <input type="password" key="repeatPassword" >
         </fieldset>
+        <button class="loginBtn" type="submit">Login</button>
+
     </form>
 </template>
 
 <style scoped>
     .input {
         padding-right: 1rem;
+    }
+    .loginBtn{
+        font-size: 1.25rem;
+        background-color: greenyellow;
+        text-align: center;
+        border: 0.05rem;
+        margin-top: 1rem;
+        display: flex;
+        border-radius: 0.25rem;
+    }
+    .loginBtn:hover{
+        background-color: green;
+        color: white;
     }
 </style>
