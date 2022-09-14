b1924e5cc0a8e76f454cf7c24f4344a1d681785f
AtanasTachev <tachev_a@yahoo.com>
2022-09-14 22:36:58 +0300

    add adrresses from free api

--- firstVueProject/vue-project/src/components/Contacts.vue
+++ firstVueProject/vue-project/src/components/Contacts.vue
@@ -1,12 +1,41 @@
 <template>
-<div class="row">
-    <h3 class="item">Address</h3>
-    <h3 class="item">Phone</h3>
-    <h3 class="item">Email</h3>
-    <h3 class="item">Webpage</h3>
-    <h3 class="item">Twitter</h3>
+<div class="row" v-for="(unit, index) in data">
+  <b-card>
+    <b-container>
+      <b-col class="item">Place ID: {{ unit.place_id}}</b-col>
+      <b-col class="item">OSM type: {{ unit.osm_type }}</b-col>
+      <b-col class="item">OSM ID: {{ unit.osm_id}}</b-col>
+      <b-col class="item">Bounding Box: {{ unit.boundingbox }}</b-col>
+      <b-col class="item">Latitude: {{ unit.lat }}</b-col>
+      <b-col class="item">Display Name: {{ unit.display_name }}</b-col>
+      <b-col class="item">Class: {{ unit.class }}</b-col>
+      <b-col class="item">Type: {{ unit.type }}</b-col>
+      <b-col class="item">Importance: {{ unit.importance }}</b-col>
+    </b-container>
+  </b-card>
 </div>
 </template>
+
+<script setup>
+  import { ref } from 'vue'
+  const API_URL = 'https://nominatim.openstreetmap.org/search.php?city=sofia&format=json'
+
+  const data = ref(null)
+  const error = ref(null)
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+  //     'X-RapidAPI-Host': 'canada-property-data.p.rapidapi.com'
+  //   }
+  // }
+
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((json) => (data.value = json))
+    .catch((err) => (error.value = err))
+  // console.log(JSON.parse(data))
+</script>
 
 <style scoped>
 .item {
